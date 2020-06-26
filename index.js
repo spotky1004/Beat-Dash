7987144ad5374ce2b03b0717efc816950da8dc67
Spotky1004 <spotky1004@gmail.com>
2020-06-27 02:36:40 +0900

    another UI

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 $(function (){
   tickNow = 0;
+  stageSelected = 0;
   gameStarted = false;
 
   function markStage() {
-    screenWidth = $(window).width();
-    screenHeight = $(window).height();
-    $('#levelBlocks').css('margin-left', (screenWidth/2-(screenHeight/10+10)*4.5) + 'px');
-    $('.stageBlock').css({height: (screenHeight/10+10) + 'px', width: (screenHeight/10+10) + 'px', 'background-size': (screenHeight/10+10) + 'px ' + (screenHeight/10+10) + 'px'})
     for (var i = 0; i < 81; i++) {
       $('.stageBlcokImg:eq(' + i + ')').removeClass('stageLocked').removeClass('stageOpened').removeClass('stageCompeleted').removeClass('perfectCompeleted').removeClass('speedCompeleted').removeClass('masterCompeleted').addClass(((levelOpened[i] == 5) ? 'masterCompeleted' : ((levelOpened[i] == 4) ? 'speedCompeleted' : ((levelOpened[i] == 3) ? 'perfectCompeleted' : ((levelOpened[i] == 2) ? 'stageCompeleted' : ((levelOpened[i] == 1) ? 'stageOpened' : 'stageLocked' )) ))));
       $('.stageBlock:eq(' + i + ')').css('background-image', 'url(Image/Bridge/' + ((levelOpened[i-9] >= 1 && i > 8 && levelOpened[i] >= 1) ? '1' : '0') + ((levelOpened[i+1] >= 1 && i%9 != 8 && levelOpened[i] >= 1) ? '1' : '0') + ((levelOpened[i+9] >= 1 && i < 72 && levelOpened[i] >= 1) ? '1' : '0') + ((levelOpened[i-1] >= 1 && i%9 != 0 && levelOpened[i] >= 1) ? '1' : '0') + '.png)');
       ((leverPointer[i] == 0) ? $('.stageBlock:eq(' + i + ')').css('opacity', '0') : 0 );
+      if (levelOpened[i] >= 1) {
+        $('.stageBlcokImg:eq(' + i + ')').css('background-image', 'url(Image/Stage/' + Number(leverPointer[i]) + '.png)')
+      }
     }
+  }
+  function thingsPositionSet() {
+    screenWidth = $(window).width();
+    screenHeight = $(window).height();
+    $('#levelBlocks').css('margin-left', (screenWidth/2-(screenHeight/10+10)*4.5) + 'px');
+    $('.stageBlock').css({height: (screenHeight/10+10) + 'px', width: (screenHeight/10+10) + 'px', 'background-size': (screenHeight/10+10) + 'px ' + (screenHeight/10+10) + 'px'});
+  }
+  function openDialog() {
+    $('#levelDialog').show();
+    $('#levelDialogBg').show();
+  }
+  function closeDialog() {
+    $('#levelDialog').hide();
+    $('#levelDialogBg').hide();
   }
 
   setInterval( function () {
@@ -19,7 +33,8 @@ $(function (){
     tickNow++;
   }, 33);
   setInterval( function () {
-    $('#gameLevelSelect').css('background-image', 'url(http://api.thumbr.it/whitenoise-361x370.png?background=2b2b2bff&noise=525252&density=' + ((tickNow%17)/5+21) + '&opacity=55)');
+    /* $('#gameLevelSelect').css('background-image', 'url(http://api.thumbr.it/whitenoise-361x370.png?background=2b2b2bff&noise=525252&density=' + ((tickNow%17)/5+21) + '&opacity=55)'); */
+    thingsPositionSet();
   }, 200);
 
   $(document).on('click','#startButton',function() {
@@ -28,7 +43,7 @@ $(function (){
       audio.play();
       audio = new Audio('Song/main.mp3');
       backgroundSound = setInterval( function (){
-        audio.play();
+        /* audio.play(); */
       }, 3000);
       gameStarted = true;
       $('#gameMainScreen').attr({
@@ -55,6 +70,18 @@ $(function (){
       }, 1700);
       /* clearTimeout(backgroundSound); */
     }
+  });
+  $(document).on('click','.stageBlcokImg',function() {
+    indexThis = $('.stageBlcokImg').index(this);
+    stageSelected = indexThis;
+    console.log(stageSelected);
+    if (levelOpened[stageSelected] >= 1) {
+      openDialog();
+    }
+  });
+  $(document).on('click','#closeDialog',function() {
+    stageSelected = 0;
+    closeDialog();
   });
 
   for (var i = 0; i < 9; i++) {
